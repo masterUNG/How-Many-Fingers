534923092e69eb3308ce4221f871f4c40f70e345
masterUNG <phrombutr@gmail.com>
2017-05-04 01:24:45 +0700

    Success

--- How Many Finger/ViewController.swift
+++ How Many Finger/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var intTextField = 0
+    
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var shewTextLabel: UILabel!
+    
+    @IBAction func guessButton(_ sender: Any) {
+        
+        let strTextField = numberTextField.text
+        
+        intTextField = Int(strTextField!)!
+        print("intTextField ==> \(intTextField)")
+        
+        let intRandom = Int(arc4random_uniform(6))
+        print("intRandom ==> \(intRandom)")
+        
+        if intTextField == intRandom {
+            shewTextLabel.text = "You Win"
+        }   else    {
+            shewTextLabel.text = "My Random ==> " + String(intRandom)
+        }
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   // videDidLoad
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   // didReceive
 
 
-}
+}   // Main Class
 
